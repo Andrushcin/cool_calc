33c11e9509108af20f4d0af2e683d211ace9abd6
Andrushcin <97093324+Andrushcin@users.noreply.github.com>
2022-06-05 00:19:19 +0300

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,31 +1,33 @@
 var res = document.getElementById("disp");
 
 window.onclick = function(Event) {
-	var but = Event.target;
+	if (Event.target.className == 'but') {
+		var but = Event.target;
 
-	if (but.innerHTML == "^") {
-    	var symbol = '**';
-	} else {
-		var symbol = but.innerHTML;
-	}
-	if (symbol == '=') {
-		try {
-			res.innerHTML = eval(res.innerHTML);
-		} catch (err) {
-			res.innerHTML = "Error";
-		}
-	} else {
-		if (symbol == "Del") {
-			res.innerHTML = '0';
+		if (but.innerHTML == "^") {
+	    	var symbol = '**';
 		} else {
-    		if (res.innerHTML == '0') {
-    			if (symbol != ".") {
-    				res.innerHTML = symbol;
-    			}
-    		} else {
-		    	var old_result = res.innerHTML;
-		    	res.innerHTML = old_result + symbol;
+			var symbol = but.innerHTML;
+		}
+		if (symbol == '=') {
+			try {
+				res.innerHTML = eval(res.innerHTML);
+			} catch (err) {
+				res.innerHTML = "Error";
+			}
+		} else {
+			if (symbol == "Del") {
+				res.innerHTML = '0';
+			} else {
+	    		if (res.innerHTML == '0') {
+	    			if (symbol != ".") {
+	    				res.innerHTML = symbol;
+	    			}
+	    		} else {
+			    	var old_result = res.innerHTML;
+			    	res.innerHTML = old_result + symbol;
+			    }
 		    }
 	    }
-    }
+	}
 };
