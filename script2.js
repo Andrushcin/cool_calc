7b56fa80470154afed0cff0d7c9a5bb127ddf0b3
Andrushcin <97093324+Andrushcin@users.noreply.github.com>
2022-06-08 13:38:07 +0300

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,30 +1,68 @@
-var res = document.getElementById("disp");
+var disp = document.getElementById("disp");
+var res_field = document.getElementById("res_field");
+var history_ = [];
+
+function process_expr(expr) {
+	try {
+		var result = eval(expr);
+	} catch (err) {
+		var result = "Error";
+	}
+	return result;
+}
 
 window.onclick = function(Event) {
 	if (Event.target.className == 'but') {
 		var but = Event.target;
-
-		if (but.innerHTML == "^") {
-	    	var symbol = '**';
-		} else {
-			var symbol = but.innerHTML;
-		}
-		if (symbol == '=') {
-			try {
-				res.innerHTML = eval(res.innerHTML);
-			} catch (err) {
-				res.innerHTML = "Error";
+		res_field.style.fontSize = '7vw';
+		
+		for (let i = 0; i < 20; i++) {
+			if (but.innerHTML == "10^") {
+				var symbol = '10**';
+		    	break;
 			}
-		} else {
-			if (symbol == "Del") {
-				res.innerHTML = '0';
+			if (but.innerHTML == "^") {
+		    	var symbol = '**';
+		    	break;
 			} else {
-	    		if ((res.innerHTML == '0' && symbol != ".") || (res.innerHTML == "Error")) {
-	    				res.innerHTML = symbol;
-	    		} else {
-			    	var old_result = res.innerHTML;
-			    	res.innerHTML = old_result + symbol;
-			    }
+				var symbol = but.innerHTML;
+				break;
+			}
+		}
+		for (let i = 0; i < 20; i++) {
+			if (symbol == '=') {
+				math_expr = res_field.innerHTML;
+				disp.innerHTML = math_expr;
+				var result = process_expr(math_expr);
+				res_field.innerHTML = result;
+				res_field.style.fontSize = '10vw';
+
+				var history_elem = String(math_expr+"<br>"+"="+result+"<br>");
+				history_.push(history_elem);
+				var hist = "";
+				for (let i = 0; i < history_.length;  i++) {
+					hist = hist + history_[i];
+				}
+				disp.innerHTML = hist;
+				break;
+			} 
+			if (symbol == "Del") {
+				res_field.innerHTML = '0';
+				break;
+			} 
+			if (symbol == 'AC') {
+				disp.innerHTML = "";
+				res_field.innerHTML = "0";
+				history_ = [];
+				break;
+			} 
+    		if ((res_field.innerHTML == '0' && symbol != ".") || (res_field.innerHTML == "Error")) {
+    			res_field.innerHTML = symbol;
+    			break;
+    		} else {
+		    	var old_result = res_field.innerHTML;
+		    	res_field.innerHTML = old_result + symbol;
+		    	break;
 		    }
 	    }
 	}
